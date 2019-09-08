e6d7535165acfe9b9f58fb18f799cb5a21f7dca7
Svet <stsupryk@gmail.com>
2019-09-08 14:27:21 +0300

    Updates

---
+++ precache-manifest.b18fab19f9db9c312e8a6792588fef56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "439d1ba7c91777cc2268cbe02c7d6fa5",
+    "url": "/React-Form/index.html"
+  },
+  {
+    "revision": "c61705820e3a88e2b059",
+    "url": "/React-Form/static/css/main.911acd26.chunk.css"
+  },
+  {
+    "revision": "410e5d8634a078d19536",
+    "url": "/React-Form/static/js/2.7327ad2d.chunk.js"
+  },
+  {
+    "revision": "c61705820e3a88e2b059",
+    "url": "/React-Form/static/js/main.b0c15e68.chunk.js"
+  },
+  {
+    "revision": "f921695a6f4ba6896e57",
+    "url": "/React-Form/static/js/runtime~main.2753d982.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-Form/static/media/logo.5d5d9eef.svg"
+  }
+]);
